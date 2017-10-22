316179e51fba0628947ebae0422808b63d4c154f
Fábio Tramasoli <fabio@tramasoli.com>
2017-10-22 12:22:56 -0200

    Creates sample Tk app

---
+++ 01.tkinterapp/app.py
@@ -0,0 +1,5 @@
+import tkinter
+
+root = tkinter.Tk()
+tkinter.Label(root, text='Hello World!').pack()
+root.mainloop()
---
+++ 02.digitalclock/app.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import font
+import datetime
+
+
+def quit(*args):
+    root.destroy()
+
+
+def clock_time():
+    time = datetime.datetime.now().strftime('%H:%M:%S')
+    txt.set(time)
+    root.after(1000,clock_time)
+
+
+root = Tk()
+root.attributes('-fullscreen',False)
+root.configure(background='black')
+root.bind('x',quit)
+fnt = font.Font(family='Helvetica', size=120, weight='bold')
+txt = StringVar()
+lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground='white', background='black')
+lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+lbl.pack(expand=True)
+clock_time()
+root.mainloop()
